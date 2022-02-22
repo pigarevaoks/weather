d5862ae04edb847d1b70e3c7c2f3d0dcfb019d1c
Климова Оксана Викторовна <OViKlimova@sberbank.ru>
2022-02-22 09:40:14 +0300

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { Hello } from "./components/Hello";
+
+const App = () => {
+  return (
+    <div>
+      <p>Oksana best girl in the world</p>
+      <Hello compiler="Parcel" framework="React" />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("app"));
